f057e03da275e206c6b18c53713b2789070a70bb
corsacca <corsacca@gmail.com>
2025-09-24 14:17:52 +0100

    v0.1.3

--- disciple-tools-conversations.php
+++ disciple-tools-conversations.php
@@ -5,7 +5,7 @@
  * Description: Track conversations that happen with your contacts.
  * Text Domain: disciple-tools-conversations
  * Domain Path: /languages
- * Version:  0.1.2
+ * Version:  0.1.3
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-conversations
  * Requires at least: 4.7.0
@@ -250,20 +250,6 @@ if ( !function_exists( 'dt_hook_ajax_notice_handler' ) ){
 }
 
 /**
- * Plugin Releases and updates
- * @todo Uncomment and change the url if you want to support remote plugin updating with new versions of your plugin
- * To remove: delete the section of code below and delete the file called version-control.json in the plugin root
- *
- * This section runs the remote plugin updating service, so you can issue distributed updates to your plugin
- *
- * @note See the instructions for version updating to understand the steps involved.
- * @link https://github.com/DiscipleTools/disciple-tools-conversations/wiki/Configuring-Remote-Updating-System
- *
- * @todo Enable this section with your own hosted file
- * @todo An example of this file can be found in (version-control.json)
- * @todo Github is a good option for delivering static json.
- */
-/**
  * Check for plugin updates even when the active theme is not Disciple.Tools
  *
  * Below is the publicly hosted .json file that carries the version information. This file can be hosted
@@ -272,21 +258,21 @@ if ( !function_exists( 'dt_hook_ajax_notice_handler' ) ){
  * Also, see the instructions for version updating to understand the steps involved.
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
-//add_action( 'plugins_loaded', function (){
-//    if ( is_admin() && !( is_multisite() && class_exists( "DT_Multisite" ) ) || wp_doing_cron() ){
-//        // Check for plugin updates
-//        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-//            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
-//                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-//            }
-//        }
-//        if ( class_exists( 'Puc_v4_Factory' ) ){
-//            Puc_v4_Factory::buildUpdateChecker(
-//                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-conversations/master/version-control.json',
-//                __FILE__,
-//                'disciple-tools-conversations'
-//            );
-//
-//        }
-//    }
-//} );
+add_action( 'plugins_loaded', function (){
+    if ( ( is_admin() || wp_doing_cron() ) && !( is_multisite() && class_exists( 'DT_Multisite' ) ) ){
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' ) ){
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-conversations/master/version-control.json',
+                __FILE__,
+                'disciple-tools-conversations'
+            );
+
+        }
+    }
+} );
